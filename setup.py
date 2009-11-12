bfabf05f384cea84894ebf91f0c9649ebc757f2c
Jannis Leidel <jannis@leidel.info>
2009-11-12 22:14:57 +0100

    Added manifest template and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+import pycompletion
+
+setup(
+    name='pycompletion',
+    version=pycompletion.__version__,
+    description='Enhancements to virtualenv',
+    author='Arthur Koziel, Jannis Leidel',
+    author_email='arthur@arthurkoziel.com, jannis@leidel.info',
+    url='http://github.com/jezdez/pycompletion/',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+    ],
+    packages=['pycompletion'],
+    package_data={'pycompletion': ['*/*']},
+    scripts=['scripts/pycompletion'],
+    zip_safe=False,
+),
