ccf6248986e5347c9cf50178c44e52185b283a61
Jannis Leidel <jannis@leidel.info>
2009-11-12 23:42:40 +0100

    Minor fixes on readme and added a long description

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
+import os
 from distutils.core import setup
-
 import pycompletion
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='pycompletion',
     version=pycompletion.__version__,
     description='A lib to collect command line completion scripts of Python packages',
+    long_description=read('README'),
     author='Arthur Koziel, Jannis Leidel',
     author_email='arthur@arthurkoziel.com, jannis@leidel.info',
     url='http://github.com/jezdez/pycompletion/',
